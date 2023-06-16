6c8c45ce3d146b5cd62b4c9fc921b6aed176a895
一護 武田 <220256@jc-21.jp>
2023-06-16 11:51:05 +0900

    まだできてない

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,9 @@
 //インクルード
 #include <Windows.h>
 #include "Direct3D.h"
-
-#include "Dice.h"
+#include "Quad.h"
 #include "Camera.h"
 #include "Dice.h"
-#include "Dice.h"
-#include "Dice.h"
-
-
-
-
-
 
 
 //定数宣言
@@ -22,137 +14,121 @@ const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
 //プロトタイプ宣言
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-//ポインタ
-Dice*pDice;
+
+//Quad* pQuad;
+Dice* pDice;
+
 
 //エントリーポイント
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
-   
 	//ウィンドウクラス（設計図）を作成
-    WNDCLASSEX wc;
-    wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
-    wc.hInstance = hInstance;                   //インスタンスハンドル
-    wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
-    wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
-    wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
-    wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);   //マウスカーソル
-    wc.lpszMenuName = NULL;                     //メニュー（なし）
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //背景（白）
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
+	wc.hInstance = hInstance;                   //インスタンスハンドル
+	wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
+	wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
+	wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
+	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);   //マウスカーソル
+	wc.lpszMenuName = NULL;                     //メニュー（なし）
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //背景（白）
+	RegisterClassEx(&wc); //クラスを登録
+
+	//ウィンドウサイズの計算
+	RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+	AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);
+	int winW = winRect.right - winRect.left;     //ウィンドウ幅
+	int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
+
+	//ウィンドウを作成
+	HWND hWnd = CreateWindow(
+		WIN_CLASS_NAME,         //ウィンドウクラス名
+		"サンプルゲーム",     //タイトルバーに表示する内容
+		WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
+		CW_USEDEFAULT,       //表示位置左（おまかせ）
+		CW_USEDEFAULT,       //表示位置上（おまかせ）
+		winW,               //ウィンドウ幅
+		winH,               //ウィンドウ高さ
+		NULL,                //親ウインドウ（なし）
+		NULL,                //メニュー（なし）
+		hInstance,           //インスタンス
+		NULL                 //パラメータ（なし）
+	);
+
+	//ウィンドウを表示
+	ShowWindow(hWnd, nCmdShow);
+
+	//Direct3D初期化
+	HRESULT hr;
+	hr = Direct3D::Initialize(winW, winH, hWnd);
+	if (FAILED(hr))
+	{
+		PostQuitMessage(0); //エラー起きたら強制終了
+	}
+
+	Camera::Initialize();
 
 
-    RegisterClassEx(&wc);  //クラスを登録
 
-    //ウィンドウサイズの計算
-    RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
-    AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);
-    int winW = winRect.right - winRect.left;     //ウィンドウ幅
-    int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
+	//pQuad = new Quad;
+	//pQuad->Initialize();
+	pDice = new Dice;
+	pDice->Initialize();
 
-    //ウィンドウを作成
-    HWND hWnd = CreateWindow(
-        WIN_CLASS_NAME,         //ウィンドウクラス名
-        "サンプルゲーム",     //タイトルバーに表示する内容
-        WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
-        CW_USEDEFAULT,       //表示位置左（おまかせ）
-        CW_USEDEFAULT,       //表示位置上（おまかせ）
-        800,                 //ウィンドウ幅
-        600,                 //ウィンドウ高さ
-        NULL,                //親ウインドウ（なし）
-        NULL,                //メニュー（なし）
-        hInstance,           //インスタンス
-        NULL                 //パラメータ（なし）
-    );
+	//メッセージループ（何か起きるのを待つ）
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT)
+	{
+		//メッセージあり
+		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 
-    //ウィンドウを表示
-    ShowWindow(hWnd, nCmdShow);
+		//メッセージなし
+		else
+		{
+			Camera::Update();
 
-    
+			//ゲームの処理
+			Direct3D::BeginDraw();
 
-    HRESULT hr;//ここから下はhr使える
-    hr=Direct3D::Initialize(WINDOW_WIDTH, WINDOW_HEIGHT, hWnd);
-    if (FAILED(hr))
-    {
-        PostQuitMessage(0);
-    }
-
-    Camera::Initialize();//カメラ初期化
-    
-   
+			//描画処理
+			static float a = 0;
+			a += 0.1;
+			XMMATRIX matR = XMMatrixRotationY(XMConvertToRadians(a));
+			XMMATRIX matT = XMMatrixTranslation(2, 0, 0);
+			XMMATRIX matS = XMMatrixScaling(2.0, 2.0, 2.0);
+			XMMATRIX mat = matR * matS * matT;
+			//pQuad->Draw(mat);
+			pDice->Draw(mat);
 
 
-   pDice = new Dice;
-    hr=pDice->Initialize();
-    if (FAILED(hr))
-    {
-        PostQuitMessage(0);
-    }
-    
+			Direct3D::EndDraw();
 
-    //メッセージループ（何か起きるのを待つ）
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-        //メッセージあり
-        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+		}
+	}
+	//SAFE_DELETE(pQuad);
+	SAFE_DELETE(pDice);
+	Direct3D::Release();
 
-        //メッセージなし
-        else
-        {
-             Camera::Update();
-            //ゲームの処理
-            Direct3D::BeginDraw();
-
-            static float a = 0;
-            a +=0.1;
-
-            /*static float b = 0;
-            b += 0.01;*/
-            
-            //XMMATRIX matH = XMMatrixRotationZ(XMConvertToRadians(b));
-            XMMATRIX matR = XMMatrixRotationY(XMConvertToRadians(a));
-           /* XMMATRIX matT = XMMatrixRotationX(XMConvertToRadians(b));*/
-            
-            //XMMATRIX matW = XMMatrixTranslation(4, 0, 0);
-           // XMMATRIX matS = XMMatrixScaling(1, 3, 1);//拡小
-            XMMATRIX matST = matR;
-           pDice->Draw(matST);
-           //pDice->Draw(matST);
-           
-            //pCamera->Release();
-
-            
-            //描画処理
-            Direct3D::EndDraw();
-
-
-        }
-    }
-    SAFE_RELEASE(pDice);
-    SAFE_DELETE(pDice);
-    Direct3D::Release();
 	return 0;
 }
 
 //ウィンドウプロシージャ（何かあった時によばれる関数）
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch (msg)
-    {
- 
-   
-    case WM_DESTROY:
-        PostQuitMessage(0);  //プログラム終了
-        return 0;
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);  //プログラム終了
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
--- Sprite.cpp
+++ Sprite.cpp
@@ -19,7 +19,7 @@ Sprite::Sprite() :
 //デストラクタ
 Sprite::~Sprite()
 {
-	Release();
+	Release();//SAFE_RELEASEシリーズに書き換えよう
 }
 
 HRESULT  Sprite::Initialize()
@@ -33,44 +33,98 @@ HRESULT  Sprite::Initialize()
 
 	}
 
-		
-		InitVertexData();
-		if (FAILED(CreateVertexBuffer()))
-		{
-			return E_FAIL;
+	// 頂点データ用バッファの設定
+	/*D3D11_BUFFER_DESC bd_vertex;
+	bd_vertex.ByteWidth = sizeof(vertices);
+	bd_vertex.Usage = D3D11_USAGE_DEFAULT;
+	bd_vertex.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd_vertex.CPUAccessFlags = 0;
+	bd_vertex.MiscFlags = 0;
+	bd_vertex.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA data_vertex;
+	data_vertex.pSysMem = vertices;*/
 
-		}
-
-	
 	
 	
 
 
 	//インデックス情報
-	
+	int index[] = {
+		0,2,3, 0,1,2,//正
+		4,6,7, 4,5,6,//右
+		8,10,11, 8,9,10,//左
+		12,14,15, 12,13,14,//上
+		19,17,16,  19,18,17,//下
+		21,23,22, 21,20,23//後
+	};
 
-	
+	// インデックスバッファを生成する
+	D3D11_BUFFER_DESC   bd;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(index);
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = index;
+	InitData.SysMemPitch = 0;
+	InitData.SysMemSlicePitch = 0;
 
 	
 
 	// コンスタントバッファ作成
+	D3D11_BUFFER_DESC cb;
+	cb.ByteWidth = sizeof(CONSTANT_BUFFER);
+	cb.Usage = D3D11_USAGE_DYNAMIC;
+	cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cb.MiscFlags = 0;
+	cb.StructureByteStride = 0;
+
+	// コンスタントバッファの作成
 	
+	pTexture_ = new Texture;
+	pTexture_->Load("Assets\\Sprite.png");
+
+	return S_OK;
 }
 
 void Sprite::Draw(XMMATRIX& worldMatrix)
 {
 	// コンスタントバッファに渡す情報
 
-		
+	CONSTANT_BUFFER cb;
+	/*cb.matWVP = XMMatrixTranspose(worldMatrix * Camera::GetViewMatrix() * Camera::GetProjectionMatrix());*/
+	
+
+	D3D11_MAPPED_SUBRESOURCE pdata;
+	Direct3D::pContext_->Map(pConstantBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &pdata);	// GPUからのデータアクセスを止める
+	memcpy_s(pdata.pData, pdata.RowPitch, (void*)(&cb), sizeof(cb));	// データを値を送る
+
+	ID3D11SamplerState* pSampler = pTexture_->GetSampler();
+	Direct3D::pContext_->PSSetSamplers(0, 1, &pSampler);
+	ID3D11ShaderResourceView* pSRV = pTexture_->GetSRV();
+	Direct3D::pContext_->PSSetShaderResources(0, 1, &pSRV);
+
+	Direct3D::pContext_->Unmap(pConstantBuffer_, 0);	//再開
 
 	//頂点バッファ
-	
+	UINT stride = sizeof(VERTEX);
+	UINT offset = 0;
+	Direct3D::pContext_->IASetVertexBuffers(0, 1, &pVertexBuffer_, &stride, &offset);
+
 	// インデックスバッファーをセット
-	
+	stride = sizeof(int);
+	offset = 0;
+	Direct3D::pContext_->IASetIndexBuffer(pIndexBuffer_, DXGI_FORMAT_R32_UINT, 0);
+
 	//コンスタントバッファ
+	Direct3D::pContext_->VSSetConstantBuffers(0, 1, &pConstantBuffer_);	//頂点シェーダー用	
+	Direct3D::pContext_->PSSetConstantBuffers(0, 1, &pConstantBuffer_);	//ピクセルシェーダー用
 
 
-	
+	Direct3D::pContext_->DrawIndexed(36, 0, 0);//数書いてる
 
 }
 
@@ -82,11 +136,34 @@ void Sprite::Release()
 	SAFE_RELEASE(pConstantBuffer_);
 	SAFE_RELEASE(pIndexBuffer_);
 	SAFE_RELEASE(pVertexBuffer_);
-	
+	/*pVertexBuffer_->Release();
+	 pIndexBuffer_->Release();
+	pConstantBuffer_->Release();*/
 
 }
 
 
 
+HRESULT Sprite::CreateVertexBuffer()
+{
+	return E_NOTIMPL;
+}
 
+HRESULT Sprite::CreateConstantBuffer()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT Sprite::LoadTexture()
+{
+	return E_NOTIMPL;
+}
+
+void Sprite::PassDateToCB(DirectX::XMMATRIX& worldMatrix)
+{
+}
+
+void Sprite::SetBufferToPipeline()
+{
+}
 
