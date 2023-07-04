0d19594e56306cd284e6f804b7cbde35f0a5cbab
武田一護 <220256@jc-21.jp>
2023-07-04 14:30:10 +0900

    完成してない

--- Main.cpp
+++ Main.cpp
@@ -138,7 +138,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 			Direct3D::EndDraw();
 
-			if (Input::IsKeyDown(DIK_ESCAPE))
+			/*if (Input::IsKeyDown(DIK_ESCAPE))
 			{
 				static int cnt = 0;
 				cnt++;
@@ -146,7 +146,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 				{
 					PostQuitMessage(0);
 				}
-			}
+			}*/
 
 
 			/*if (Input::IsKeyDown(DIK_A))
