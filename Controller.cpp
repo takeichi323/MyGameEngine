17e4c80458d7b5bba1eee1f1d96ec5855bd44002
武田一護 <220256@jc-21.jp>
2023-08-29 15:17:53 +0900

    上下も回転できた

--- Controller.cpp
+++ Controller.cpp
@@ -1,93 +1,118 @@
 #include "Controller.h"
-#include "Engine/Input.h"
 #include "Engine/Camera.h"
+#include "Engine/Input.h"
 
-
-
-Controller::Controller(GameObject* parent) :GameObject(parent, "Controller")
+//コンストラクタ
+Controller::Controller(GameObject* parent)
+    :GameObject(parent, "Controller")
 {
-    /*Camera::SetPosition(XMFLOAT3(7, 6, -5));*/
-    Camera::SetTarget(XMFLOAT3(7, 2, 1));
+    transform_.position_.x = 7.0f;
+    transform_.position_.z = 7.0f;
+
+    transform_.rotate_.x = 45.0f;
 }
 
-
-
+////デストラクタ
 //Controller::~Controller()
 //{
 //}
 
-
-
+//初期化
 void Controller::Initialize()
 {
-    transform_.position_.x = 7;
 }
 
-
-
+//更新
 void Controller::Update()
 {
-
-
-
-
-    //if (Input::IsKey(DIK_UP))
-    //    transform_.rotate_.y += 1;
-    //if (Input::IsKey(DIK_DOWN))
-    //    transform_.rotate_.y -= 1;
-    if (Input::IsKey(DIK_RIGHT))
-        transform_.rotate_.x += 1;
+    //左回転
     if (Input::IsKey(DIK_LEFT))
-        transform_.rotate_.x -= 1;
+    {
+        transform_.rotate_.y -= 1.0f;
+    }
+
+    //右回転
+    if (Input::IsKey(DIK_RIGHT))
+    {
+        transform_.rotate_.y += 1.0f;
+    }
+
+    //回転行列
+    XMMATRIX mRotateY = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));
 
 
 
-    //現在地をベクトル型に変換
+    if (Input::IsKey(DIK_UP))
+    {
+        transform_.rotate_.x += 1.0f;
+        if (transform_.rotate_.x > 89)
+        {
+            transform_.rotate_.x = 89;
+        }
+    }
+
+    if (Input::IsKey(DIK_DOWN))
+    {
+        transform_.rotate_.x -= 1.0f;
+        if (transform_.rotate_.x < 0)
+        {
+            transform_.rotate_.x = 0;
+        }
+
+
+    }
+    //回転行列
+    XMMATRIX mRotateX = XMMatrixRotationX(XMConvertToRadians(transform_.rotate_.x));
+
+
+
+
+
+
+    //現在位置をベクトルにしておく
     XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
-    //1フレームの移動ベクトル
-    XMVECTOR vMove = { 0.0f, 0.0f, 0.1f, 0.0f };  //奥に0.1m
-    //transform_.rotate_.x度回転させる行列を作成
-    XMMATRIX mRotX = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.x));
-    //移動ベクトルを変形（本体と同じ向きに回転）させる
-    vMove = XMVector3TransformCoord(vMove, mRotX);
 
-
-
-
-    if (Input::IsKey(DIK_W)) {
-        //移動
-        vPos += vMove;
-        //現在地をベクトルからいつものtransform_.position_に戻す
-        XMStoreFloat3(&transform_.position_, vPos);
-        transform_.position_.z += 0.1f;
+    //前後移動
+    XMVECTOR frontMove = XMVectorSet(0, 0, 0.1f, 0);                //奥向きのXMVECTOR構造体を用意し
+    frontMove = XMVector3TransformCoord(frontMove, mRotateY);        //移動ベクトル回転
+    if (Input::IsKey(DIK_W))
+    {
+        vPos += frontMove;
     }
     if (Input::IsKey(DIK_S))
-        transform_.position_.z -= 0.1f;
+    {
+        vPos -= frontMove;
+    }
+
+    //左右移動
+    XMVECTOR rightMove = XMVectorSet(0.1f, 0, 0, 0);                //右向きのXMVECTOR構造体を用意し
+    rightMove = XMVector3TransformCoord(rightMove, mRotateY);        //移動ベクトル回転
+
     if (Input::IsKey(DIK_D))
-        transform_.position_.x += 0.1f;
+    {
+        vPos += rightMove;
+    }
     if (Input::IsKey(DIK_A))
-        transform_.position_.x -= 0.1f;
+    {
+        vPos -= rightMove;
+    }
+
+    XMStoreFloat3(&transform_.position_, vPos);
 
 
-
-
-
-    Camera::SetTarget(transform_.position_);
-    XMVECTOR vCam = { 0, 5, -10, 0 };  //四次元
-    vCam = XMVector3TransformCoord(vCam, mRotX);
-    XMFLOAT3 camPos;
-    XMStoreFloat3(&camPos, vPos + vCam);
-    Camera::SetPosition(camPos);
+    //カメラ
+    XMVECTOR vCam = XMVectorSet(0, 0, -10, 0);     //自撮り棒用意
+    vCam = XMVector3TransformCoord(vCam, mRotateX * mRotateY);  //自撮り棒回転
+    Camera::SetPosition(vPos + vCam);           //カメラの位置は自撮り棒の先端（現在地+自撮り棒）
+    Camera::SetTarget(transform_.position_);    //カメラの見る位置はこのオブジェクトの位置
 }
 
-
-
+//描画
 void Controller::Draw()
 {
 }
 
-
-
+//開放
 void Controller::Release()
 {
 }
