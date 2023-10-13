66b714d9e9abfdbff68d05b11fe679c06ff82e6a
武田一護 <220256@jc-21.jp>
2023-10-13 11:40:09 +0900

    変化なし

--- resource.h
+++ resource.h
@@ -15,13 +15,15 @@
 #define ID_MENU_NEW                     40004
 #define ID_MENU_OPEN                    40005
 #define ID_MENU_SAVE                    40006
+#define ID_40007                        40007
+#define ID_40008                        40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
